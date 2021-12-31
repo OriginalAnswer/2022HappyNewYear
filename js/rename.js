b2ea7c61df7cac0d7a055052e3cfe445fa50ed6d
OriginalAnswer <akiseonair@gmail.com>
2021-12-31 14:54:46 +0900

    update

--- js/rename.js
+++ js/rename.js
@@ -1,7 +1,5 @@
 const rename = document.querySelector('#letterPage__form-rename');
 
-// const LETTERNAME_KEY = "lettername";
-
 function renameSet() {     
     localStorage.removeItem(LETTERNAME_KEY)
 }
